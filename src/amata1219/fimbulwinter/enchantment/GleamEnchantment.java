2415aae113874a78e2a7a3a621dd25df5b9df362
amata1219 <suzukiamata@gmail.com>
2021-01-07 22:13:56 +0900

    :sparkles: 無効果のエンチャントを追加し魔界

---
+++ src/amata1219/fimbulwinter/enchantment/GleamEnchantment.java
@@ -0,0 +1,55 @@
+package amata1219.fimbulwinter.enchantment;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+
+public class GleamEnchantment extends Enchantment {
+
+    public static final GleamEnchantment INSTANCE = new GleamEnchantment();
+
+    private GleamEnchantment() {
+        super(NamespacedKey.minecraft("gleam"));
+    }
+
+    @Override
+    public String getName() {
+        return "gleam";
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 0;
+    }
+
+    @Override
+    public EnchantmentTarget getItemTarget() {
+        return EnchantmentTarget.VANISHABLE;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return false;
+    }
+
+    @Override
+    public boolean conflictsWith(Enchantment enchantment) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(ItemStack itemStack) {
+        return false;
+    }
+}
